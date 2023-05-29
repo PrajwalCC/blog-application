7b5f098a309350a4289f99f03d7d8e58f2cb4984
PrajwalCC <b220035@iiit-bh.ac.in>
2023-05-29 11:34:01 +0530

    updating cors code

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ import cors from "cors";
 
 const app = express()
 
-app.use(cors());
+
+// app.use(cors());
+app.use(
+    cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+})
+);
+app.options(
+    "*",
+cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+})
+);
+
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
